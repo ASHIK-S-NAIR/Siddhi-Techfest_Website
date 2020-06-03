9d4b2b44bfab571140dac5b710145942b9144ce6
Ashik S Nair <aashiq5342@gmail.com>
2020-06-03 23:37:37 +0530

    navbar corrected, handbuger animation done, sidebar problem not fixed

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function openFunction(){
-    document.getElementById('menu').style.width = "300px";
-}
+// function openFunction(){
+//     document.getElementById('menu').style.width = "300px";
+// }
 
-function closeFunction(){
-    document.getElementById('menu').style.width = "0px";
-}
+// function closeFunction(){
+//     document.getElementById('menu').style.width = "0px";
+// }
+
+const menuBtn =document.querySelector('.menu-btn');
+let menuOpen =false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen){
+        menuBtn.classList.add('open');
+        menuOpen=true;
+        document.getElementById('menu').style.width = "350px";
+    }else{
+        menuBtn.classList.remove('open');
+        menuOpen=false;
+        document.getElementById('menu').style.width = "0px";
+    }
+});
